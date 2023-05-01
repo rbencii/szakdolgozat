438f77da96bc4db554d3b602093f3c55982227e5
rbencii <l705qg@inf.elte.hu>
2023-05-01 20:52:25 +0200

    simple transition

--- tobbjatekos-kartyajatek-keretrendszer/src/components/card.tsx
+++ tobbjatekos-kartyajatek-keretrendszer/src/components/card.tsx
@@ -12,7 +12,7 @@ export default function Card({card, style}: {card: CardData, style?: any}){
 
     card.suit === "Diamonds" ? (icon = "&diams;") : (icon = "&" + card.suit.toLowerCase() + ";");
 
-    return(<div className="w-6 text-center" style={style}>
+    return(<div className="w-6 text-center select-none cursor-pointer hover:scale-125 active:duration-0 hover:duration-300 hover:text-green-500 active:text-green-200 transition-all duration-700" style={style}>
         {card.value}<span dangerouslySetInnerHTML={{__html:icon}}></span>
         </div>)
     
